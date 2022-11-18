641fdcacb1a0bab4923e5e871e389bf4d411dc3d
BlazerXXI <ruslandendik@gmail.com>
2022-11-19 01:45:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+const mySkills = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("square-animation");
+    }
+  });
+});
+mySkills.observe(document.querySelector(".my-skills__list"));
+
+const aboutMe = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("about-animation");
+    }
+  });
+});
+aboutMe.observe(document.querySelector(".about-me__text-block"));
+
+const hero = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("rev-square-animation");
+    }
+  });
+});
+hero.observe(document.querySelector(".hero__container"));
+
+// slider
+const dots = document.querySelectorAll(".portfolio-dots");
+const slider = document.querySelectorAll(".portfolio-works__list");
+
+// Select tab content item
+function selectItem(e) {
+  removeActive();
+  removeShow();
+  this.classList.add("_active");
+  const slide = document.querySelector(`#${this.id}-content`);
+
+  // Add show class
+  slide.classList.add("show");
+}
+
+function removeActive() {
+  dots.forEach((item) => item.classList.remove("_active"));
+}
+function removeShow() {
+  slider.forEach((item) => item.classList.remove("show"));
+}
+
+//  Listen for tab click
+dots.forEach((item) => item.addEventListener("click", selectItem));
